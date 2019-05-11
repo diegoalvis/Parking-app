3def4ac383643ebda7a3cc8e55b7db45e2f0df89
Ricardo <ricardo.vejarano.garcia@gmail.com>
2019-05-11 10:01:20 -0500

    Pagos pendientes con los sub items centrados y no alineados a la izquierda

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         theme: AppTheme.build(),
         initialRoute: '/',
         routes: {
-          '/': (context) => MapPage(),
+          '/': (context) => BillPage(),
           '/login': (context) => LoginPage(),
           '/register-phone': (context) => PhonePage(),
           '/register-one': (context) => RegisterOnePage(),
--- lib/pages/bill/bill_page.dart
+++ lib/pages/bill/bill_page.dart
@@ -6,22 +6,173 @@ void main() => runApp(BillPage());
 class BillPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      color: Colors.white,
-      child: Row(
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home: BillContainer(),
+    );
+  }
+}
+
+class BillContainer extends StatefulWidget {
+  _BillContainerState createState() => _BillContainerState();
+}
+
+class _BillContainerState extends State<BillContainer> {
+  @override
+  Widget build(BuildContext context) {
+
+    final pagosPendientes = Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 20.0,
+            left: 15.0
+          ),
+          child: Text(
+            "Pagos Pendientes",
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black54,
+              fontWeight: FontWeight.w700
+            ),
+            textAlign: TextAlign.left,
+          ),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          margin: EdgeInsets.only(
+            top: 20.0,
+            left: 15.0
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Expanded(child: new DrawerOnly()),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 10.0
+                ),
+
+                child: new Align(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: 0.0
+                        ),
+                        child: Text(
+                          "COP 500",
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        alignment: Alignment.centerLeft,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: 10.0
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(
+                                right: 15.0
+                              ),
+                              child: Icon(
+                                Icons.visibility
+                              ),
+                            ),
+                            Container(
+                              child: Text("ABC 123"),
+                            ),
+                          ],  
+                        ),
+                        
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.topLeft,
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    right: 25.0
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                          "05/05/2019 ",
+                        style: TextStyle(
+                          fontSize: 12.0
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                          "10:35pm",
+                        style: TextStyle(
+                          fontSize: 8.0
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+                alignment: Alignment.topLeft,
+              ),
             ],
           ),
-          Text(
-            " Bill page",
-            style: Theme.of(context).textTheme.display1,
-          )
-        ],
-      ),
+          alignment: Alignment.centerLeft,
+        ),
+      ],
     );
+
+    final facturas = Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+              top: 20.0,
+              left: 15.0
+          ),
+          child: Text(
+            "Facturas",
+            style: TextStyle(
+                fontSize: 18.0,
+                fontFamily: 'Roboto',
+                color: Colors.black54,
+                fontWeight: FontWeight.w700
+            ),
+            textAlign: TextAlign.left,
+          ),
+        )
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mis Facturas",
+          style: TextStyle(
+              color: Colors.black
+          ),
+        ),
+       backgroundColor: Colors.white,
+      ),
+      body: new Container(
+       color: Theme.of(context).canvasColor,
+       child: Column(
+          children: <Widget>[
+            Container(
+                child: pagosPendientes
+            ),
+            Container(
+                child: facturas
+            ),
+          ],
+        ),
+      )
+      );
+
   }
 }
