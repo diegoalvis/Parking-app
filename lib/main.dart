c5a0b5ce91bb91bdebe9df052ecde14b25fbc316
anamarin <anamarinmartinez0904@outlook.es>
2019-05-15 13:26:55 -0300

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         theme: AppTheme.build(),
         initialRoute: '/',
         routes: {
-          '/': (context) => ReportPage(),
+          '/': (context) => MainPage(),
           '/login': (context) => LoginPage(),
           '/register-phone': (context) => PhonePage(),
           '/register-one': (context) => RegisterOnePage(),
