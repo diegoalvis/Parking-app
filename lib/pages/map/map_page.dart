f0174b1dced0eb919f2645b1706e4d5404cb346b
Ricardo <ricardo.vejarano.garcia@gmail.com>
2019-05-21 11:12:36 -0500

    Marker from zones bloc
    Zones from list of zones

--- lib/pages/map/map_page.dart
+++ lib/pages/map/map_page.dart
@@ -34,6 +34,8 @@ class _MapContainerState extends State<MapContainer>{
   Completer<GoogleMapController> _controller = Completer();
   bool flagZones = false;
 
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
   static final CameraPosition _initialPosition = CameraPosition(
     target: LatLng(6.151379, -75.615247),
     zoom: 17.0,
@@ -57,6 +59,7 @@ class _MapContainerState extends State<MapContainer>{
           if (state is SuccessState<List<Zone>>) {
             zones = state.data;
             this.flagZones = true;
+            _addMarker();
           }
           return Stack(
             children: <Widget>[
@@ -66,12 +69,29 @@ class _MapContainerState extends State<MapContainer>{
                 onMapCreated: (GoogleMapController controller) {
                   _controller.complete(controller);
                 },
+                markers: Set<Marker>.of(markers.values),
               )
             ],
           );
         },
       ),
     );
+  }
+
+  void _addMarker() {
+    if(this.flagZones == true) {
+      for(var i = 0 ; i < this.zones.length ; i++) {
+        final MarkerId markerId = MarkerId(zones[i].idZone);
+        final Marker marker = Marker(
+          markerId: markerId,
+          position: LatLng(zones[i].lat, zones[i].lon),
+        );
+        // adding a new marker to map
+        markers[markerId] = marker;
+      }
+    }
+
+
   }
 
 }
